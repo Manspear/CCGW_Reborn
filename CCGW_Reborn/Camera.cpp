4ad8b131a96fdc7eda2c278cb05984c1f79db92a
emil palm <carlemilpalm@gmail.com>
2016-05-09 13:44:46 +0200

    asdad

--- CCGW_Reborn/Camera.cpp
+++ CCGW_Reborn/Camera.cpp
@@ -7,9 +7,9 @@
 void Camera::follow( glm::vec3 position, const glm::vec3 &lookDir, float distance, const glm::vec3 &up )
 {
 	glm::vec3 temp = {lookDir.x, 0, lookDir.z};
-	 
-	mPosition = position - distance * lookDir + glm::vec3(glm::cross(lookDir, glm::cross(glm::vec3(0,1,0),lookDir)))*(distance/4)+ glm::cross(temp, glm::vec3(0,1,0));// (-lookDir * distance);
-	glm::vec3 lookingPoint = position + lookDir * distance;
+	glm::vec3 offset = glm::vec3(glm::cross(lookDir, glm::cross(glm::vec3(0, 1, 0), lookDir)))*(distance / 4);
+	mPosition = position - distance * lookDir + offset+ glm::cross(temp, glm::vec3(0,1,0));// (-lookDir * distance);
+	glm::vec3 lookingPoint = position + offset + lookDir * distance;
 	mView = glm::lookAt(mPosition , lookingPoint, up);
 	//std::cout << lookingPoint.x << "   " << lookingPoint.y << "   " << lookingPoint.z << "\n";
 }
